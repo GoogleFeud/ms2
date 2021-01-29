29a35f2a81534cf2df05c363c7caee2bc750bc93
GoogleFeud <volen.sl666@gmail.com>
2021-01-29 15:06:08 +0200

    Some more tests

--- tests/compiler_context/FUNCTIONS.test.ts
+++ tests/compiler_context/FUNCTIONS.test.ts
@@ -25,6 +25,32 @@ describe("COMPILER CONTEXT FUNCTIONS", () => {
         Evaler.clear().interpret(ctx.result);
         expect(Evaler.stack.pop().call(undefined, 1, 5)).to.be.equal(6);
     });
+
+    it("Nested function", () => {
+        const ctx = new CompilerContext({bufferSize: 1000});
+        ctx.addOpCode(OP_CODES.FN_START);
+        const fnSizeOffset = ctx.skip(2);
+        ctx.enterBlock();
+        ctx.addOpCode(OP_CODES.FN_START_INNER);
+        ctx.addUnsigned8(0);
+        const innerFnSizeOffset = ctx.skip(2);
+        ctx.enterBlock();
+        ctx.addOpCode(OP_CODES.PUSH_VAR); ctx.addUnsigned16(0);
+        ctx.addOpCode(OP_CODES.PUSH_VAR); ctx.addUnsigned16(1);
+        ctx.addOpCode(OP_CODES.ADD);
+        ctx.addOpCode(OP_CODES.RETURN);
+        const innerFnSize = ctx.exitBlock();
+        ctx.addOpCode(OP_CODES.FN_END_INNER);
+        ctx.addUnsigned8(0);
+        ctx.result.writeUInt16BE(innerFnSize, innerFnSizeOffset);
+        ctx.addOpCode(OP_CODES.RETURN);
+        const fnSize = ctx.exitBlock();
+        ctx.addOpCode(OP_CODES.FN_END);
+        ctx.result.writeUInt16BE(fnSize, fnSizeOffset);
+        ctx.addOpCode(OP_CODES.END);
+        Evaler.clear().interpret(ctx.result);
+        expect(Evaler.stack.pop().call().call(undefined, 1, 6)).to.be.equal(7);
+    });
     
 });
 
